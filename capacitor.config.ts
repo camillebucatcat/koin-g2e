cebc3f2aa9666f34c5f1e80819a9a158bdf451d0
jusjasjaz <jazminelean.dimagiba.xentra@gmail.com>
2023-09-05 21:44:18 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'koin-g2e',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
